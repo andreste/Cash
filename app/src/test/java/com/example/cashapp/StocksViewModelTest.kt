b3458adc0e8822fe8dc7d9fb010348078134c916
Andres Trevino <andreste24@gmail.com>
2024-08-21 17:19:31 -0400

    Add tests

--- app/src/test/java/com/example/cashapp/StocksViewModelTest.kt
+++ app/src/test/java/com/example/cashapp/StocksViewModelTest.kt
@@ -1,80 +1,146 @@
 package com.example.cashapp
 
+import com.example.cashapp.model.Stock
+import com.example.cashapp.model.StocksResponse
+import com.example.cashapp.repository.StocksRepository
+import com.example.cashapp.viewmodel.StocksViewModel
+import com.example.cashapp.viewmodel.ViewState
+import io.mockk.coEvery
+import io.mockk.junit4.MockKRule
+import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.test.*
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
+import retrofit2.Response
 
+@FlowPreview
 @ExperimentalCoroutinesApi
 class StocksViewModelTest {
 
     @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
+    val mockkRule = MockKRule(this)
 
-    private val dispatcher = StandardTestDispatcher()
-
-    @Mock
-    private lateinit var observer: Observer<StocksViewModel.StocksUiState>
-
-    @Mock
-    private lateinit var stockRepository: StockRepository // Mock the repository
-
+    private val stocksRepository: StocksRepository = mockk<StocksRepository>()
     private lateinit var viewModel: StocksViewModel
 
+    private val testDispatcher = UnconfinedTestDispatcher()
+    private val testScope = TestScope(testDispatcher)
+
     @Before
-    fun setup(){
-        MockitoAnnotations.openMocks(this)
-        Dispatchers.setMain(dispatcher)
-        viewModel = StocksViewModel(stockRepository) // Inject the mock repository
-        viewModel.uiState.observeForever(observer)
-    }
-
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain()
-        viewModel.uiState.removeObserver(observer)
+    fun setup() {
+        viewModel = StocksViewModel(stocksRepository)
+        Dispatchers.setMain(testDispatcher)
     }
 
     @Test
-    fun `when viewModel is initialized, uiState should be Loading`() = runTest {
-        assert(viewModel.uiState.value is StocksViewModel.StocksUiState.Loading)
+    fun `getStocks sets state to Content when repository returns stocks`() = testScope.runTest {
+        // Arrange
+        val mockStocks = listOf(
+            Stock(
+                ticker = "AAPL",
+                name = "Apple",
+                currency = "USD",
+                currentPriceCents = 100,
+                quantity = 10,
+                currentPriceTimestamp = 1234567890
+            ),
+            Stock(
+                ticker = "GOOG",
+                name = "Google",
+                currency = "USD",
+                currentPriceCents = 200,
+                quantity = 2,
+                currentPriceTimestamp = 12567890
+            )
+        )
+        val mockResponse = mockRetrofitResponse(mockStocks)
+        coEvery { stocksRepository.getStocks() } returns mockResponse
+
+        assert((viewModel.state.value as ViewState.Content).list.isEmpty())
+
+        // Act
+        viewModel.getStocks()
+
+        // Assert
+        assert((viewModel.state.value as ViewState.Content).list.size == 2)
+        assert((viewModel.state.value as ViewState.Content).list[0].name == "Apple")
+        assert((viewModel.state.value as ViewState.Content).list[1].name == "Google")
     }
 
     @Test
-    fun `when fetching stocks is successful, uiState should be Success`() = runTest {
-        // Mock successful stock data retrieval
-        val stockData = listOf(/* Your stock data objects */)
-        // when(stockRepository.getStocks()).thenReturn(stockData)
+    fun `getStocks sets state to Content when repository returns stocks, but filters stocks with null quantity`() =
+        testScope.runTest {
+            // Arrange
+            val mockStocks = listOf(
+                Stock(
+                    ticker = "AAPL",
+                    name = "Apple",
+                    currency = "USD",
+                    currentPriceCents = 100,
+                    quantity = 10,
+                    currentPriceTimestamp = 1234567890
+                ),
+                Stock(
+                    ticker = "GOOG",
+                    name = "Google",
+                    currency = "USD",
+                    currentPriceCents = 200,
+                    quantity = null,
+                    currentPriceTimestamp = 12567890
+                )
+            )
+            val mockResponse = mockRetrofitResponse(mockStocks)
+            coEvery { stocksRepository.getStocks() } returns mockResponse
 
-        viewModel.fetchStocks()
+            assert((viewModel.state.value as ViewState.Content).list.isEmpty())
 
-        assert(viewModel.uiState.value is StocksViewModel.StocksUiState.Success)
-        // Further assertions to check if the success state contains the correct data
-        // val successState = viewModel.uiState.value as StocksViewModel.StocksUiState.Success
-        // assertEquals(successState.stocks, stockData)
+            // Act
+            viewModel.getStocks()
+
+            // Assert
+            assert((viewModel.state.value as ViewState.Content).list.size == 1)
+            assert((viewModel.state.value as ViewState.Content).list[0].name == "Apple")
+        }
+
+    @Test
+    fun `getStocks sets state to Content with an empty list`() = testScope.runTest {
+        // Arrange
+        val mockStocks = emptyList<Stock>()
+        val mockResponse = mockRetrofitResponse(mockStocks)
+        coEvery { stocksRepository.getStocks() } returns mockResponse
+
+        assert((viewModel.state.value as ViewState.Content).list.isEmpty())
+
+        // Act
+        viewModel.getStocks()
+
+        // Assert
+        assert((viewModel.state.value as ViewState.Content).list.isEmpty())
     }
 
     @Test
-    fun `when fetching stocks fails, uiState should be Error`() = runTest {
-        // Mock failed stock data retrieval
-        val exception = Exception("Network error")
-        // when(stockRepository.getStocks()).thenThrow(exception)
+    fun `getStocks sets state to Error when repository returns error`() = testScope.runTest {
+        // Arrange
+        coEvery { stocksRepository.getStocks() } returns mockErrorResponse()
 
-        viewModel.fetchStocks()
+        // Act
+        viewModel.getStocks()
 
-        assert(viewModel.uiState.value is StocksViewModel.StocksUiState.Error)
-        // Further assertions to check if the error state contains the correct exception
-        // val errorState = viewModel.uiState.value as StocksViewModel.StocksUiState.Error
-        // assertEquals(errorState.exception, exception)
+        // Assert
+        assert(viewModel.state.value is ViewState.Error)
+        assert((viewModel.state.value as ViewState.Error).message == "Could not get stocks")
+    }
+
+    private fun mockRetrofitResponse(stocks: List<Stock>): Response<StocksResponse> {
+        return Response.success(StocksResponse(stocks))
+    }
+
+    private fun mockErrorResponse(): Response<StocksResponse> {
+        return Response.error(500, "Error".toResponseBody())
     }
 }
